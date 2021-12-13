fb1e90e6a3c09ccce05cb7e6e9115d927119df80
Nujanauss <70958669+Nujanauss@users.noreply.github.com>
2021-12-13 14:06:33 +0100

    Use folder not files

---
+++ mentalab-explore/loadcsv.m
@@ -0,0 +1,68 @@
+function [EEG, com] = loadcsv(filepath, eeg_srate)
+    com = '';
+    EEG = [];
+    EEG = eeg_emptyset;
+
+    orn_srate = 20; % Sampling rate of ORN data
+
+    [directory, filename, ~] = fileparts(filepath);
+    filename_split = split(filename, "_");
+    name = char(filename_split(1));
+
+    EXG = importdata([directory '\' name '_ExG.csv'], ',', 1);
+    eeg_data = sortrows(EXG.data, 1);
+    eeg_data = eeg_data(:, 2:end)';
+    eeg_ch_names = EXG.textdata(2:end);
+    eeg_timestamps = EXG.data(:, 1);
+    
+    ORN = importdata([directory '\' name '_ORN.csv'], ',', 1);
+    orn_data = sortrows(ORN.data, 1);
+    orn_data = orn_data(:, 2:end)';
+    orn_ch_names = ORN.textdata(2:end);
+    orn_timestamps = ORN.data(:, 1);
+    
+    marker = importdata([directory '\' name '_Marker.csv'], ',',1);
+    eeg_marker = [];
+    orn_marker = [];
+    if isfield(marker,'data')
+        eeg_marker = zeros(size(marker.data,1), 2);
+        eeg_marker(:, 2) = marker.data(:, 2);
+    
+        orn_marker = zeros(size(marker.data,1), 2);
+        orn_marker(:, 2) = marker.data(:, 2);
+    end
+    
+    % Event syncing
+    if isfield(marker, 'data')
+        for i = 1:size(marker.data, 1)
+            eeg_marker(i, 1) = find(eeg_timestamps > marker.data(i,1), 1);    
+            orn_marker(i, 1)= find(orn_timestamps > marker.data(i,1), 1);
+        end
+    end
+    
+    % Convert to EEGLAB structure
+    eeg_chanlocs = struct('labels', eeg_ch_names);
+    EEG = pop_importdata('dataformat', 'array', 'nbchan', 0, 'data', ...
+        eeg_data, 'setname', 'raw_eeg', 'srate', eeg_srate, 'chanlocs', ...
+        eeg_chanlocs, 'pnts', 0, 'xmin', 0);
+    EEG = eeg_checkset(EEG);
+    EEG = pop_importevent( EEG, 'event', eeg_marker, 'fields', ...
+        {'latency', 'type'}, 'timeunit', NaN);
+    EEG = eeg_checkset(EEG);
+    
+    orn_chanlocs = struct('labels', orn_ch_names);
+    ORN = pop_importdata('dataformat', 'array', 'nbchan', 0, 'data', ...
+        orn_data, 'setname', 'raw_orn', 'srate', orn_srate, 'chanlocs', ...
+        orn_chanlocs, 'pnts', 0, 'xmin', 0);
+    ORN = eeg_checkset(ORN);
+    ORN = pop_importevent(ORN, 'event', orn_marker, 'fields', ...
+        {'latency', 'type'}, 'timeunit', NaN);
+    ORN = eeg_checkset(ORN);
+    
+    % Save data to disk
+    EEG = pop_saveset(EEG, 'filename', strcat(filename, '_ExG.set'), 'filepath', directory);
+    EEG = eeg_checkset(EEG);
+    
+    ORN = pop_saveset(ORN, 'filename', strcat(filename, '_ORN.set'), 'filepath', directory);
+    ORN = eeg_checkset(ORN);
+end    
